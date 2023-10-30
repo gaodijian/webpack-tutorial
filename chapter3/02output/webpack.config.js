c88a1b6babea5a40129c2d84aebcf8caa3bc8660
gaodijian <gaodijian@foxmail.com>
2023-10-30 15:22:00 +0800

    add example about the 'output' configuration.

---
+++ chapter3/02output/webpack.config.js
@@ -0,0 +1,18 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+module.exports = {
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "https://cdn.myapp.com/assets/",
+  },
+  mode: "development",
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+      filename: "index.html",
+      inject: "body",
+    }),
+  ],
+};
